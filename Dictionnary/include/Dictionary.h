a210bcb57c0ffe45b5edf9540f654dfbe7d3313b
Skilgannon <skilganon77@gmail.com>
2017-09-16 20:01:54 +0200

    check for accent character

--- Dictionnary/include/Dictionary.h
+++ Dictionnary/include/Dictionary.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <list>
 #include <string>
+#include <cctype>
 
 #include <utility>
 #include <algorithm>
@@ -15,32 +16,41 @@
 
 using DictValuePair = std::vector<std::list<std::string>>;
 using Dict = std::map<char, DictValuePair>;
+using AccentSet = std::unordered_set<unsigned char>;
+
+#define MIN_SIZE	2
+#define MAX_SIZE	25
 
 class Dictionary
 {
 	std::string		m_dictPath;
-	unsigned int	m_MinLength;
-	unsigned int	m_MaxLength;
 	Dict			m_Dict;
+	unsigned int	m_MinSize;
+	unsigned int	m_MaxSize;
 
-	std::unordered_set<char>	m_Accents = {};
+	static const std::string				Ref;
+	static const std::string				ResFolder;
+	static const std::map<char, AccentSet>	Accents;
 
 public:
-	static const std::string REF;
-	static const std::string RES_FOLDER;
-
-public:
-	Dictionary(const std::string& dictPath = "DefaultDict.txt", unsigned int minLength = 2, unsigned int maxLength = 25);
+	Dictionary(const std::string& dictPath = "DefaultDict.txt", unsigned int minSize = MIN_SIZE, unsigned int maxSize = MAX_SIZE);
 	~Dictionary();
 
 	void				load(const std::string& dictPath = "");
+
+	void				setMinSize(const unsigned int& size);
+	void				setMaxSize(const unsigned int& size);
+
 	const Dict&			getDict() const;
-	const unsigned int&	getMinLength() const;
-	const unsigned int&	getMaxLength() const;
-	const unsigned int	getLength() const;
+	const unsigned int&	getMinSize() const;
+	const unsigned int&	getMaxSize() const;
+	const unsigned int	getSize() const;
 
 private:
+	void	strip(std::string& str);
+	bool	isValid(const std::string& line);
 	void	formatLine(std::string& line);
+	void	addWord(const std::string& word);
 };
 
 std::ostream&	operator<<(std::ostream& os, const Dictionary& dict);
--- Dictionnary/src/Dictionary.cpp
+++ Dictionnary/src/Dictionary.cpp
@@ -2,45 +2,94 @@
 
 using namespace std;
 
-Dictionary::Dictionary(const string& dictPath, unsigned int minLength, unsigned int maxLength) :
+Dictionary::Dictionary(const string& dictPath, unsigned int minSize, unsigned int maxSize) :
 	m_dictPath(dictPath),
-	m_MinLength(minLength),
-	m_MaxLength(maxLength),
 	m_Dict()
 {
-	vector<list<string>> tmp((m_MaxLength - m_MinLength), list<string>());
-	for (auto& c : Dictionary::REF)
+	setMinSize(minSize);
+	setMaxSize(maxSize);
+
+	vector<list<string>> tmp((m_MaxSize - m_MinSize), list<string>());
+	for (auto& c : Dictionary::Ref)
 		m_Dict.emplace(c, tmp);
 }
 
 Dictionary::~Dictionary()
 {}
 
+void Dictionary::setMinSize(const unsigned int& size)
+{
+	m_MinSize = (size < MIN_SIZE ? MIN_SIZE : size);
+}
+
+void Dictionary::setMaxSize(const unsigned int& size)
+{
+	m_MaxSize = (size > MAX_SIZE ? MAX_SIZE : size);
+}
+
+void Dictionary::strip(string& str)
+{
+	str.erase(remove_if(str.begin(), str.end(), isspace), str.end());
+}
+
+bool Dictionary::isValid(const string& line)
+{
+	if (line.empty() || isupper(line.at(0))
+		|| line.size() < m_MinSize || line.size() > m_MaxSize)
+		return false;
+	return true;
+}
+
 void Dictionary::formatLine(string& line)
 {
 	for (auto& c : line)
 	{
-		if ()
+		if (isspace(c))
+			continue;
+
+		unsigned char tmp = static_cast<unsigned char>(c);
+		for (const auto& kv : Dictionary::Accents)
+		{
+			if (kv.second.find(tmp) != kv.second.end())
+			{
+				c = kv.first;
+				break;
+			}
+		}
 	}
-	line.erase(remove_if(line.begin(), line.end(), isspace), line.end());
+	this->strip(line);
+}
+
+void Dictionary::addWord(const string& word)
+{
+
 }
 
 void Dictionary::load(const string& dictPath)
 {
 	if (!dictPath.empty())
 		m_dictPath = dictPath;
-	string path = (Dictionary::RES_FOLDER + m_dictPath);
+	string path = (Dictionary::ResFolder + m_dictPath);
 
 	ifstream file(path);
 	if (!file.is_open())
-		throw invalid_argument(path + " is not a valid path");
+		throw invalid_argument("Dictionary: cannot open file: " + path);
 
+	cout << "Loading dictionary from file: " << path << endl;
 	string line;
 	while (file.good())
 	{
 		getline(file, line);
+		this->strip(line);
+		if (!this->isValid(line))
+			continue;
 		this->formatLine(line);
+		if (line.find_first_not_of(Dictionary::Ref) != line.npos)
+			continue;
+		this->addWord(line);
 	}
+	file.close();
+	cout << "Dictionary loaded! Ready to use!" << endl;
 }
 
 const Dict&	Dictionary::getDict() const
@@ -48,19 +97,19 @@ const Dict&	Dictionary::getDict() const
 	return m_Dict;
 }
 
-const unsigned int& Dictionary::getMinLength() const
+const unsigned int& Dictionary::getMinSize() const
 {
-	return m_MinLength;
+	return m_MinSize;
 }
 
-const unsigned int& Dictionary::getMaxLength() const
+const unsigned int& Dictionary::getMaxSize() const
 {
-	return m_MaxLength;
+	return m_MaxSize;
 }
 
-const unsigned int Dictionary::getLength() const
+const unsigned int Dictionary::getSize() const
 {
-	return m_MaxLength - m_MinLength;
+	return m_MaxSize - m_MinSize;
 }
 
 ostream& operator<<(ostream& os, const Dictionary& dict)
@@ -75,5 +124,14 @@ ostream& operator<<(ostream& os, const Dictionary& dict)
 	return os;
 }
 
-const string Dictionary::REF = "abcdefghijklmnopqrstuvwxyz";
-const string Dictionary::RES_FOLDER = "../res/";
+const string Dictionary::Ref = "abcdefghijklmnopqrstuvwxyz";
+const string Dictionary::ResFolder = "./res/";
+const map<char, AccentSet> Dictionary::Accents = {
+	{'a', {u'à', u'â', u'ä', u'ã'}},
+	{'e', {u'è', u'é', u'ê', u'ë'}},
+	{'i', {u'î', u'ï'}},
+	{'o', {u'ô', u'ö', u'õ'}},
+	{'u', {u'ù', u'û', u'ü'}},
+	{'c', {u'ç'}},
+	{' ', {u'-'}}
+};
